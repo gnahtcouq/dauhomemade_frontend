dba5e56b79878a0dba5b5694b96f5d501a6822a6
Quoc Thang <comehere.thang@gmail.com>
2024-09-07 09:29:31 +0700

    Tạo http client để gọi API
    Tạo http client để gọi API

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import {clsx, type ClassValue} from 'clsx'
+import {twMerge} from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+/* 
+Xoá đi ký tự `/` đầu tiên của path
+*/
+export function normalizePath(path: string) {
+  return path.startsWith('/') ? path.slice(1) : path
+}
+
+const isBrowser = typeof window !== 'undefined'
+
+export const getAccessTokenFromLocalStorage = () =>
+  isBrowser ? localStorage.getItem('accessToken') : null
+
+export const getRefreshTokenFromLocalStorage = () =>
+  isBrowser ? localStorage.getItem('refreshToken') : null
+export const setAccessTokenToLocalStorage = (value: string) =>
+  isBrowser && localStorage.setItem('accessToken', value)
+
+export const setRefreshTokenToLocalStorage = (value: string) =>
+  isBrowser && localStorage.setItem('refreshToken', value)
+export const removeTokensFromLocalStorage = () => {
+  isBrowser && localStorage.removeItem('accessToken')
+  isBrowser && localStorage.removeItem('refreshToken')
 }
