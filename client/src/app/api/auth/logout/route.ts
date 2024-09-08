046b829c4b15c3e2a0f368fc71a58362a225a34f
Quoc Thang <comehere.thang@gmail.com>
2024-09-09 03:35:15 +0700

    Code route handler logout
    Code route handler logout

---
+++ client/src/app/api/auth/logout/route.ts
@@ -0,0 +1,26 @@
+import authApiRequest from '@/apiRequests/auth'
+import {cookies} from 'next/headers'
+
+export async function POST() {
+  const cookieStore = cookies()
+  const accessToken = cookieStore.get('accessToken')?.value
+  const refreshToken = cookieStore.get('refreshToken')?.value
+
+  cookieStore.delete('accessToken')
+  cookieStore.delete('refreshToken')
+
+  if (!accessToken || !refreshToken) {
+    return Response.json(
+      {message: 'Không tìm thấy access token hoặc refresh token'},
+      {status: 200}
+    )
+  }
+
+  try {
+    const result = await authApiRequest.sLogout({accessToken, refreshToken})
+
+    return Response.json(result.payload)
+  } catch (error) {
+    return Response.json({message: 'Có lỗi xảy ra'}, {status: 200})
+  }
+}
