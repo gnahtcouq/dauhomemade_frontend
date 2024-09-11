95ee1e306be4a0a286306e43bef38592b0ad6635
Quoc Thang <comehere.thang@gmail.com>
2024-09-11 21:16:51 +0700

    UI Template món ăn
    UI Template món ăn

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,10 +1,14 @@
+import authApiRequest from '@/apiRequests/auth'
+import envConfig from '@/config'
+import {DishStatus, OrderStatus, TableStatus} from '@/constants/type'
 import {toast} from '@/hooks/use-toast'
 import {EntityError} from '@/lib/http'
 import {clsx, type ClassValue} from 'clsx'
+// import {format} from 'date-fns'
+import jwt from 'jsonwebtoken'
+import {BookX, CookingPot, HandCoins, Loader, Truck} from 'lucide-react'
 import {UseFormSetError} from 'react-hook-form'
 import {twMerge} from 'tailwind-merge'
-import jwt from 'jsonwebtoken'
-import authApiRequest from '@/apiRequests/auth'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -111,4 +115,99 @@ export const checkAndRefreshToken = async (param?: {
       param?.onError && param.onError()
     }
   }
+}
+
+export const formatCurrency = (number: number) => {
+  return new Intl.NumberFormat('vi-VN', {
+    style: 'currency',
+    currency: 'VND'
+  }).format(number)
+}
+
+export const getVietnameseDishStatus = (
+  status: (typeof DishStatus)[keyof typeof DishStatus]
+) => {
+  switch (status) {
+    case DishStatus.Available:
+      return 'Có sẵn'
+    case DishStatus.Unavailable:
+      return 'Không có sẵn'
+    default:
+      return 'Ẩn'
+  }
+}
+
+export const getVietnameseOrderStatus = (
+  status: (typeof OrderStatus)[keyof typeof OrderStatus]
+) => {
+  switch (status) {
+    case OrderStatus.Delivered:
+      return 'Đã phục vụ'
+    case OrderStatus.Paid:
+      return 'Đã thanh toán'
+    case OrderStatus.Pending:
+      return 'Chờ xử lý'
+    case OrderStatus.Processing:
+      return 'Đang nấu'
+    default:
+      return 'Từ chối'
+  }
+}
+
+export const getVietnameseTableStatus = (
+  status: (typeof TableStatus)[keyof typeof TableStatus]
+) => {
+  switch (status) {
+    case TableStatus.Available:
+      return 'Có sẵn'
+    case TableStatus.Reserved:
+      return 'Đã đặt'
+    default:
+      return 'Ẩn'
+  }
+}
+
+export const getTableLink = ({
+  token,
+  tableNumber
+}: {
+  token: string
+  tableNumber: number
+}) => {
+  return (
+    envConfig.NEXT_PUBLIC_URL + '/tables/' + tableNumber + '?token=' + token
+  )
+}
+
+export function removeAccents(str: string) {
+  return str
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/đ/g, 'd')
+    .replace(/Đ/g, 'D')
+}
+
+export const simpleMatchText = (fullText: string, matchText: string) => {
+  return removeAccents(fullText.toLowerCase()).includes(
+    removeAccents(matchText.trim().toLowerCase())
+  )
+}
+
+// export const formatDateTimeToLocaleString = (date: string | Date) => {
+//   return format(
+//     date instanceof Date ? date : new Date(date),
+//     'HH:mm:ss dd/MM/yyyy'
+//   )
+// }
+
+// export const formatDateTimeToTimeString = (date: string | Date) => {
+//   return format(date instanceof Date ? date : new Date(date), 'HH:mm:ss')
+// }
+
+export const OrderStatusIcon = {
+  [OrderStatus.Pending]: Loader,
+  [OrderStatus.Processing]: CookingPot,
+  [OrderStatus.Rejected]: BookX,
+  [OrderStatus.Delivered]: Truck,
+  [OrderStatus.Paid]: HandCoins
 }
