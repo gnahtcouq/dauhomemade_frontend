96556d0ed4e2ea9de5d408cf0996a33470ca686d
Quoc Thang <comehere.thang@gmail.com>
2024-09-26 19:51:21 +0700

    Bổ sung metadata trang thanh toán thành công
    Bổ sung metadata trang thanh toán thành công

--- client/src/app/[locale]/(public)/payment/page.tsx
+++ client/src/app/[locale]/(public)/payment/page.tsx
@@ -1,4 +1,23 @@
 import PaymentSuccess from '@/app/[locale]/(public)/payment/payment-success'
+import envConfig, {Locale} from '@/config'
+import {getTranslations} from 'next-intl/server'
+
+export async function generateMetadata({
+  params: {locale}
+}: {
+  params: {locale: Locale}
+}) {
+  const t = await getTranslations({locale, namespace: 'Payment'})
+  const url = envConfig.NEXT_PUBLIC_URL + `/${locale}/payment`
+
+  return {
+    title: t('title'),
+    description: t('description'),
+    alternates: {
+      canonical: url
+    }
+  }
+}
 
 export default function OrdersPage() {
   return (
