b0ca3ae140eaa9beab8657e27cc6b5d36619973f
Quoc Thang <comehere.thang@gmail.com>
2024-09-29 11:05:07 +0700

    Bổ sung Analytics và SpeedInsights Vercel
    Bổ sung Analytics và SpeedInsights Vercel

--- client/src/components/app-provider.tsx
+++ client/src/components/app-provider.tsx
@@ -11,6 +11,8 @@ import {
 import {RoleType} from '@/types/jwt.types'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
+import {Analytics} from '@vercel/analytics/react'
+import {SpeedInsights} from '@vercel/speed-insights/next'
 import React, {useEffect, useRef} from 'react'
 import {Socket} from 'socket.io-client'
 import {create} from 'zustand'
@@ -100,6 +102,8 @@ export default function AppProvider({children}: {children: React.ReactNode}) {
     // >
     <QueryClientProvider client={queryClient}>
       {children}
+      <SpeedInsights />
+      <Analytics />
       <RefreshToken />
       <ListenLogoutSocket />
       <ReactQueryDevtools initialIsOpen={false} />
