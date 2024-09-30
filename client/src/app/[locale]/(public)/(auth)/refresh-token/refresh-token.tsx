502e84c042a317dfd3e2527cd39bbc9838e556d6
Quoc Thang <comehere.thang@gmail.com>
2024-09-30 21:03:24 +0700

    Fix bug chuyển hướng
    Fix bug chuyển hướng

--- client/src/app/[locale]/(public)/(auth)/refresh-token/refresh-token.tsx
+++ client/src/app/[locale]/(public)/(auth)/refresh-token/refresh-token.tsx
@@ -30,6 +30,7 @@ export default function RefreshToken() {
         }
       })
     } else {
+      console.log(1)
       router.push('/')
     }
   }, [router, refreshTokenFromUrl, redirectPathname])
