b79c9715a41ea529c21f02eeb8ad29e388b7e9df
Quoc Thang <comehere.thang@gmail.com>
2024-10-04 10:13:14 +0700

    Bổ sung metadata cho guest login page
    Bổ sung metadata cho guest login page

--- client/src/app/[locale]/(public)/tables/[number]/page.tsx
+++ client/src/app/[locale]/(public)/tables/[number]/page.tsx
@@ -1,4 +1,23 @@
 import GuestLoginForm from '@/app/[locale]/(public)/tables/[number]/guest-login-form'
+import envConfig, {Locale} from '@/config'
+import {getTranslations} from 'next-intl/server'
+
+export async function generateMetadata({
+  params: {locale}
+}: {
+  params: {locale: Locale}
+}) {
+  const t = await getTranslations({locale, namespace: 'GuestLogin'})
+  const url = envConfig.NEXT_PUBLIC_URL + `/${locale}/tables/`
+
+  return {
+    title: t('title'),
+    description: t('description'),
+    alternates: {
+      canonical: url
+    }
+  }
+}
 
 export default function TableNumberPage() {
   return <GuestLoginForm />
